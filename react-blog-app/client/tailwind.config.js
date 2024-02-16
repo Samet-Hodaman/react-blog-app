104f1a7524ee9ca6cd76fb4d2c845fc965921747
Samet Hodaman <samethodaman@std.iyte.edu.tr>
2024-02-16 23:21:49 +0300

    Create and run the server

--- react-blog-app/client/tailwind.config.js
+++ react-blog-app/client/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export const content = [
   "./src/**/*.{js,jsx,ts,tsx}",
+  "node_modules/flowbite-react/lib/esm/**/*.js",
 ];
 export const theme = {
   extend: {},
